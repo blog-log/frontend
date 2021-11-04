f7b303480a92bc4bbc2eef80a9a9b67f003782f0
Brandon Cate <brandoncate95@gmail.com>
2021-11-04 18:16:11 -0500

    add postgres adapter support

--- Makefile
+++ Makefile
@@ -5,6 +5,9 @@ docker-build:
 docker-run:
 	docker run -it -p 3000:3000 --env-file .env bloglog:v0.0.1
 
+compose-run:
+	docker-compose up
+
 generate-secret-dev:
 	kubectl create secret generic frontend-secret --dry-run=client --from-env-file=.env.dev -o yaml > k8s/secret-dev.yaml
 
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,23 +1,48 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import type { Adapter } from "next-auth/adapters";
+import { TypeORM } from "next-auth/adapters";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-var firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+const getAdapter = (): ReturnType<Adapter> => {
+  switch (process.env.DATABASE_TYPE) {
+    case "firebase":
+      return getFirebaseAdapter();
+    case "postgres":
+      return getTypeormAdapter();
+    default:
+      throw new Error(`unsupported database type ${process.env.DATABASE_TYPE}`);
+  }
 };
 
-const firestore = (
-  firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
-).firestore();
+function getFirebaseAdapter(): ReturnType<Adapter> {
+  var firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
+  const firestore = (
+    firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
+  ).firestore();
+
+  return FirebaseAdapter(firestore);
+}
+
+function getTypeormAdapter(): ReturnType<Adapter> {
+  return TypeORM.Adapter({
+    type: "postgres",
+    url: process.env.DATABASE_URI,
+    synchronize: true,
+  });
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -50,6 +75,6 @@ export default NextAuth({
       return token;
     },
   },
-  adapter: FirebaseAdapter(firestore),
+  adapter: getAdapter(),
   debug: true,
 });
